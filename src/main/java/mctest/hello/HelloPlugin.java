50c4adab46c70c2135092f9630529e0a15c033f0
Steve Conover <steve@squareup.com>
2015-06-05 22:19:56 -0700

    chat hook

--- src/main/java/mctest/hello/HelloPlugin.java
+++ src/main/java/mctest/hello/HelloPlugin.java
@@ -2,20 +2,18 @@ package mctest.hello;
 
 import net.canarymod.Canary;
 import net.canarymod.hook.HookHandler;
-import net.canarymod.hook.system.ServerTickHook;
+import net.canarymod.hook.player.ChatHook;
 import net.canarymod.plugin.Plugin;
 import net.canarymod.plugin.PluginListener;
 
 public class HelloPlugin extends Plugin {
   @Override public boolean enable() {
-    getLogman().info("Enabling "+ getName() + " Version " + getVersion());
-    getLogman().info("Authored by "+getAuthor());
+    getLogman().info("Enabling " + getName() + " Version " + getVersion());
+    getLogman().info("Authored by "+ getAuthor());
 
     Canary.hooks().registerListener(new Listener(), this);
 
     Canary.getServer().broadcastMessage("ENABLE PLUGIN");
-    System.out.println(String.valueOf(Canary.getServer().getTicksPerSecond()));
-
 
     return true;
   }
@@ -26,8 +24,8 @@ public class HelloPlugin extends Plugin {
 
   public static class Listener implements PluginListener {
     @HookHandler
-    public void onTick(ServerTickHook tickHook) {
-      // System.out.println("tick");
+    public void onChat(ChatHook hook) {
+      System.out.println("CHAT HOOK FIRED: " + hook.getMessage());
     }
   }
 }
